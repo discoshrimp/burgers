c88c0e6ef41c2a3a7c0269f017c5a57134a94c9f
discoshrimp <roydega@gmail.com>
2018-11-01 15:09:51 -0700

    asd

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,14 @@
 var mysql = require("mysql");
 
-if(process.env.JawsDB_URL){
-  connection=mysql.createConnection(process.env.JawsDB_URL)
+if(process.env.JAWSDB_URL){
+  connection=mysql.createConnection(process.env.JAWSDB_URL)
 }else{
   var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "ZU){63xETAPf",
-    database: "j4ey8lbsxjbawiul"
+    database: "cat_db"
   });
 }
 // Make connection.
